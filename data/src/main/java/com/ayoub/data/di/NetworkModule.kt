0666311ddb0912f040e261420595d7b6b3e49a50
Elhoucine Ayoub <ayoub@Ayoubs-MacBook-Pro.local>
2024-01-28 23:21:33 +0100

    provide wallet and country api

--- data/src/main/java/com/ayoub/data/di/NetworkModule.kt
+++ data/src/main/java/com/ayoub/data/di/NetworkModule.kt
@@ -2,6 +2,8 @@ package com.ayoub.data.di
 
 import com.ayoub.data.network.api.RecipientApi
 import com.ayoub.data.network.RemoteDataSource
+import com.ayoub.data.network.api.CountryApi
+import com.ayoub.data.network.api.WalletApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,6 +21,22 @@ class NetworkModule {
         remoteDataSource: RemoteDataSource,
     ): RecipientApi {
         return remoteDataSource.buildApi(RecipientApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideWalletApi(
+        remoteDataSource: RemoteDataSource,
+    ): WalletApi {
+        return remoteDataSource.buildApi(WalletApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCountryApi(
+        remoteDataSource: RemoteDataSource,
+    ): CountryApi {
+        return remoteDataSource.buildApi(CountryApi::class.java)
     }
 
 }
