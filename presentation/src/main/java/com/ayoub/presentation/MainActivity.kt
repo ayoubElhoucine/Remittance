e715427a224aad00b31fe27b55bc7b59137dda1a
Elhoucine Ayoub <apple@apples-MacBook-Pro.local>
2024-01-29 15:54:46 +0100

    clean code

--- presentation/src/main/java/com/ayoub/presentation/MainActivity.kt
+++ presentation/src/main/java/com/ayoub/presentation/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ayoub.presentation.ui.MyApp
-import com.ayoub.presentation.ui.theme.RemittanceTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -26,10 +18,4 @@ internal class MainActivity : ComponentActivity() {
             MyApp(finishActivity = ::finish)
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-
 }
